13a7ba45de40aa9a4bd9ccfe6d33e0f4ed88f1f9
Pierre2324 <pdbbasketball@gmail.com>
2021-12-17 20:52:46 -0500

    selinux: don't forget selinux state parameter to avc_has_perm function
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2203,7 +2203,7 @@ static inline u32 open_file_to_av(struct file *file)
 
 static int selinux_binder_set_context_mgr(const struct cred *mgr)
 {
-	return avc_has_perm(current_sid(), cred_sid(mgr), SECCLASS_BINDER,
+	return avc_has_perm(&selinux_state, current_sid(), cred_sid(mgr), SECCLASS_BINDER,
 			    BINDER__SET_CONTEXT_MGR, NULL);
 }
 
@@ -2231,7 +2231,7 @@ static int selinux_binder_transaction(const struct cred *from,
 static int selinux_binder_transfer_binder(const struct cred *from,
 					  const struct cred *to)
 {
-	return avc_has_perm(cred_sid(from), cred_sid(to),
+	return avc_has_perm(&selinux_state, cred_sid(from), cred_sid(to),
 			    SECCLASS_BINDER, BINDER__TRANSFER,
 			    NULL);
 }
