d4cd3a5148871f5360817b354ae48eb90033460f
Pranav Vashi <neobuddy89@gmail.com>
2022-09-07 07:13:00 +0530

    Revert "net: Import Xiaomi changes"
    This reverts commit 7a27532c607162e16c4819c6b2a03bd591d21a8b.

--- include/net/sock.h
+++ include/net/sock.h
@@ -455,7 +455,6 @@ struct sock {
 	u32			sk_ack_backlog;
 	u32			sk_max_ack_backlog;
 	kuid_t			sk_uid;
-	pid_t			pid_num;
 	spinlock_t		sk_peer_lock;
 	struct pid		*sk_peer_pid;
 	const struct cred	*sk_peer_cred;
