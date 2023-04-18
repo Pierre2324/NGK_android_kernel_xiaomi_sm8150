e3b11bcc68db2f0e74949a000957707cfa8c0672
Pierre2324 <pdbbasketball@gmail.com>
2023-04-17 20:52:51 -0400

    KernelSU: Add support [Part 2] Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1715,6 +1715,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1729,6 +1732,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
